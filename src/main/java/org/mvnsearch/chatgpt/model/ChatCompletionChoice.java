3156c1b18650ce3eb770cc6cc25147c4734b2941
Libing Chen <libing.chen@gmail.com>
2023-06-17 19:38:53 +0800

    build: add @JsonAlias("delta") for message

--- src/main/java/org/mvnsearch/chatgpt/model/ChatCompletionChoice.java
+++ src/main/java/org/mvnsearch/chatgpt/model/ChatCompletionChoice.java
@@ -1,9 +1,11 @@
 package org.mvnsearch.chatgpt.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ChatCompletionChoice {
     Integer index;
+    @JsonAlias("delta")
     ChatMessage message;
     @JsonProperty("finish_reason")
     String finishReason;
