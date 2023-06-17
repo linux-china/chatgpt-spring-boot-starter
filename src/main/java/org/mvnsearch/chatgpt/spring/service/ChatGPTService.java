cfc1b2ad4fc764f9ccdf2a50259a51ed0ddd80bf
Libing Chen <libing.chen@gmail.com>
2023-06-17 21:24:29 +0800

    feat: add stream()

--- src/main/java/org/mvnsearch/chatgpt/spring/service/ChatGPTService.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/service/ChatGPTService.java
@@ -2,8 +2,12 @@ package org.mvnsearch.chatgpt.spring.service;
 
 import org.mvnsearch.chatgpt.model.ChatCompletionRequest;
 import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ChatGPTService {
     Mono<ChatCompletionResponse> chat(ChatCompletionRequest request);
+
+    Flux<ChatCompletionResponse> stream(ChatCompletionRequest request);
+
 }
