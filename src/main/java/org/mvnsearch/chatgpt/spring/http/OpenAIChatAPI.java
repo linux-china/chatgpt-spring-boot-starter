04593b4e97d46d3e4b924d7cb6493aa100d19688
Libing Chen <libing.chen@gmail.com>
2023-06-17 17:55:01 +0800

    app: OpenAIChatAPI http interface

---
+++ src/main/java/org/mvnsearch/chatgpt/spring/http/OpenAIChatAPI.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.chatgpt.spring.http;
+
+import org.mvnsearch.chatgpt.model.ChatCompletionRequest;
+import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+import reactor.core.publisher.Mono;
+
+@HttpExchange("https://api.openai.com")
+public interface OpenAIChatAPI {
+    @PostExchange("/v1/chat/completions")
+    Mono<ChatCompletionResponse> chat(@RequestBody ChatCompletionRequest request);
+}
