8f10976a126a7955290d843452711b50faabb6f9
Libing Chen <libing.chen@gmail.com>
2023-06-17 18:24:22 +0800

    test: ChatGPTServiceImplTest

---
+++ src/test/java/org/mvnsearch/chatgpt/spring/service/ChatGPTServiceImplTest.java
@@ -0,0 +1,37 @@
+package org.mvnsearch.chatgpt.spring.service;
+
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.chatgpt.demo.ProjectBootBaseTest;
+import org.mvnsearch.chatgpt.model.ChatCompletionRequest;
+import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
+import org.mvnsearch.chatgpt.model.ChatMessage;
+import org.mvnsearch.chatgpt.model.FunctionCall;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ChatGPTServiceImplTest extends ProjectBootBaseTest {
+    @Autowired
+    private ChatGPTService chatGPTService;
+
+    @Test
+    public void testSimpleChat() {
+        final ChatCompletionRequest request = ChatCompletionRequest.of("What's Java Language?");
+        final ChatCompletionResponse response = chatGPTService.chat(request).block();
+        System.out.println(response.getReplyText());
+    }
+
+    @Test
+    public void testChatWithFunctions() throws Exception {
+        final ChatCompletionRequest request = ChatCompletionRequest.functions("Hi Jackie. If you have time, could you send an email to libing.chen@gmail.com and linux_china@hotmail.com and invite to join the party on tomorrow? Thanks!",
+                List.of("send_email"));
+        final ChatCompletionResponse response = chatGPTService.chat(request).block();
+        for (ChatMessage chatMessage : response.getReply()) {
+            final FunctionCall functionCall = chatMessage.getFunctionCall();
+            if (functionCall != null) {
+                final Object result = functionCall.getFunctionStub().call();
+                System.out.println(result);
+            }
+        }
+    }
+}
