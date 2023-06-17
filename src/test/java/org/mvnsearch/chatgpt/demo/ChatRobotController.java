bd5393d4c6191f69e59928e2563a00e164405896
Libing Chen <libing.chen@gmail.com>
2023-06-17 21:37:17 +0800

    feat: add streamChat

--- src/test/java/org/mvnsearch/chatgpt/demo/ChatRobotController.java
+++ src/test/java/org/mvnsearch/chatgpt/demo/ChatRobotController.java
@@ -5,9 +5,8 @@ import org.mvnsearch.chatgpt.model.ChatCompletionRequest;
 import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
 import org.mvnsearch.chatgpt.spring.service.ChatGPTService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -18,6 +17,18 @@ public class ChatRobotController {
     @PostMapping("/chat")
     public Mono<String> chat(@RequestBody String content) {
         return chatGPTService.chat(ChatCompletionRequest.of(content))
+                .map(ChatCompletionResponse::getReplyText);
+    }
+
+    /**
+     * stream chat by server-sent events
+     *
+     * @param content message content
+     * @return SSE flux
+     */
+    @GetMapping("/stream-chat")
+    public Flux<String> streamChat(@RequestParam String content) {
+        return chatGPTService.stream(ChatCompletionRequest.of(content))
                 .map(ChatCompletionResponse::getReplyText);
     }
 }
