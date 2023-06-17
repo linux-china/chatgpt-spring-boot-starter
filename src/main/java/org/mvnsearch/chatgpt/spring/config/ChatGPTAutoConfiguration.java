7738608db3816caf088df0a5cc2230f02a3d35e7
Libing Chen <libing.chen@gmail.com>
2023-06-17 21:26:51 +0800

    feat: add OpenAIChatAPI bean

--- src/main/java/org/mvnsearch/chatgpt/spring/config/ChatGPTAutoConfiguration.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/config/ChatGPTAutoConfiguration.java
@@ -19,11 +19,16 @@ import java.util.Optional;
 public class ChatGPTAutoConfiguration {
 
     @Bean
-    public ChatGPTService chatGPTService(@Value("${openai.api.key}") String openaiApiKey,
-                                         Optional<List<GPTFunctionsStub>> stubs) throws Exception {
+    public OpenAIChatAPI openAIChatAPI(@Value("${openai.api.key}") String openaiApiKey) {
         WebClient webClient = WebClient.builder().defaultHeader("Authorization", "Bearer " + openaiApiKey).build();
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builder().clientAdapter(WebClientAdapter.forClient(webClient)).build();
-        OpenAIChatAPI openAIChatAPI = httpServiceProxyFactory.createClient(OpenAIChatAPI.class);
+        return httpServiceProxyFactory.createClient(OpenAIChatAPI.class);
+
+    }
+
+    @Bean
+    public ChatGPTService chatGPTService(OpenAIChatAPI openAIChatAPI,
+                                         Optional<List<GPTFunctionsStub>> stubs) throws Exception {
         return new ChatGPTServiceImpl(openAIChatAPI, stubs.orElse(Collections.emptyList()));
     }
 }
