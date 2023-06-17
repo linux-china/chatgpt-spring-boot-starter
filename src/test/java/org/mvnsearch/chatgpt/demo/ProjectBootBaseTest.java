9f1af29bf7967c2e7c346933ae819286c76c9efe
Libing Chen <libing.chen@gmail.com>
2023-06-17 18:20:54 +0800

    test: ProjectBootBaseTest

---
+++ src/test/java/org/mvnsearch/chatgpt/demo/ProjectBootBaseTest.java
@@ -0,0 +1,8 @@
+package org.mvnsearch.chatgpt.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ChatGPTDemoApp.class)
+public abstract class ProjectBootBaseTest {
+
+}
