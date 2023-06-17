05b512af66afb37b5ae26296b977c243efab73d3
Libing Chen <libing.chen@gmail.com>
2023-06-17 21:26:28 +0800

    feat: add stream()

--- src/main/java/org/mvnsearch/chatgpt/spring/http/OpenAIChatAPI.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/http/OpenAIChatAPI.java
@@ -5,10 +5,15 @@ import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @HttpExchange("https://api.openai.com")
 public interface OpenAIChatAPI {
     @PostExchange("/v1/chat/completions")
     Mono<ChatCompletionResponse> chat(@RequestBody ChatCompletionRequest request);
+
+    @PostExchange("/v1/chat/completions")
+    Flux<ChatCompletionResponse> stream(@RequestBody ChatCompletionRequest request);
+
 }
