0559e8061634b47b93e3ac21aad14cc25b4e3e1e
Libing Chen <libing.chen@gmail.com>
2023-06-17 18:17:39 +0800

    test: ChatGPTDemoApp

---
+++ src/test/java/org/mvnsearch/chatgpt/demo/ChatGPTDemoApp.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.chatgpt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGPTDemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGPTDemoApp.class, args);
+    }
+
+}
