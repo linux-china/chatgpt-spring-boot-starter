1c389df4a5c6611fdb1be4b04a0d6f21c1ef28ef
Libing Chen <libing.chen@gmail.com>
2023-06-18 18:25:56 +0800

    test: polishing

--- src/test/java/org/mvnsearch/chatgpt/model/function/GPTFunctionUtilsTest.java
+++ src/test/java/org/mvnsearch/chatgpt/model/function/GPTFunctionUtilsTest.java
@@ -3,11 +3,16 @@ package org.mvnsearch.chatgpt.model.function;
 import org.junit.jupiter.api.Test;
 import org.mvnsearch.chatgpt.demo.service.GPTFunctions;
 
+import java.util.Map;
+
 public class GPTFunctionUtilsTest {
 
     @Test
     public void testExtractFunctions() throws Exception {
         final var functions = GPTFunctionUtils.extractFunctions(GPTFunctions.class);
-        System.out.println(GPTFunctionUtils.toFunctionsJsonArray(functions.values()));
+        for (Map.Entry<String, ChatGPTJavaFunction> entry : functions.entrySet()) {
+            final ChatGPTJavaFunction javaFuntion = entry.getValue();
+            System.out.println(javaFuntion.getJavaMethod().getName());
+        }
     }
 }
