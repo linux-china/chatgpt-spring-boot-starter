86dca50cd34069fe943b19556b3de23e8ea8fdad
Libing Chen <libing.chen@gmail.com>
2023-06-18 15:35:09 +0800

    build: add parameters

--- src/main/java/org/mvnsearch/chatgpt/model/ChatFunction.java
+++ src/main/java/org/mvnsearch/chatgpt/model/ChatFunction.java
@@ -1,9 +1,16 @@
 package org.mvnsearch.chatgpt.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChatFunction {
     private String name;
     private String description;
-    private ChatFunctionParameters parameters;
+    private Parameters parameters;
 
     public String getName() {
         return name;
@@ -21,11 +28,141 @@ public class ChatFunction {
         this.description = description;
     }
 
-    public ChatFunctionParameters getParameters() {
+    public Parameters getParameters() {
         return parameters;
     }
 
-    public void setParameters(ChatFunctionParameters parameters) {
+    public void setParameters(Parameters parameters) {
         this.parameters = parameters;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    public static class Parameters {
+        private String type;
+        private Map<String, JsonSchemaProperty> properties;
+        private List<String> required;
+
+        public Parameters() {
+        }
+
+        public Parameters(String type, Map<String, JsonSchemaProperty> properties, List<String> required) {
+            this.type = type;
+            this.properties = properties;
+            this.required = required;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public Map<String, JsonSchemaProperty> getProperties() {
+            return properties;
+        }
+
+        public void setProperties(Map<String, JsonSchemaProperty> properties) {
+            this.properties = properties;
+        }
+
+        public List<String> getRequired() {
+            return required;
+        }
+
+        public void setRequired(List<String> required) {
+            this.required = required;
+        }
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class JsonSchemaProperty {
+        @JsonIgnore
+        private String name;
+        private String type;
+        private String description;
+        /**
+         * items for type array
+         */
+        private JsonArrayItems items;
+
+        public JsonSchemaProperty() {
+        }
+
+        public JsonSchemaProperty(String name, String type, String description) {
+            this.name = name;
+            this.type = type;
+            this.description = description;
+        }
+
+        public JsonSchemaProperty(String name, String type, String description, JsonArrayItems items) {
+            this.name = name;
+            this.type = type;
+            this.description = description;
+            this.items = items;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public JsonArrayItems getItems() {
+            return items;
+        }
+
+        public void setItems(JsonArrayItems items) {
+            this.items = items;
+        }
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class JsonArrayItems {
+        private String type;
+        private String description;
+
+        public JsonArrayItems() {
+        }
+
+        public JsonArrayItems(String type, String description) {
+            this.type = type;
+            this.description = description;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
     }
 }
