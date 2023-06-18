2929b1430e748410ef89c28a6933d75d6ac585c7
Libing Chen <libing.chen@gmail.com>
2023-06-18 15:33:52 +0800

    refactor: dropped

--- src/main/java/org/mvnsearch/chatgpt/spring/service/ChatGPTServiceImpl.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/service/ChatGPTServiceImpl.java
@@ -1,9 +1,9 @@
 package org.mvnsearch.chatgpt.spring.service;
 
 import org.mvnsearch.chatgpt.model.*;
+import org.mvnsearch.chatgpt.model.function.ChatGPTJavaFunction;
 import org.mvnsearch.chatgpt.model.function.GPTFunctionUtils;
 import org.mvnsearch.chatgpt.model.function.GPTFunctionsStub;
-import org.mvnsearch.chatgpt.model.function.JsonSchemaFunction;
 import org.mvnsearch.chatgpt.spring.http.OpenAIChatAPI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,16 +17,16 @@ import java.util.Map;
 public class ChatGPTServiceImpl implements ChatGPTService {
     private static final Logger log = LoggerFactory.getLogger(ChatGPTServiceImpl.class);
     private final OpenAIChatAPI openAIChatAPI;
-    private final Map<String, JsonSchemaFunction> allJsonSchemaFunctions = new HashMap<>();
+    private final Map<String, ChatGPTJavaFunction> allJsonSchemaFunctions = new HashMap<>();
     private final Map<String, ChatFunction> allChatFunctions = new HashMap<>();
 
     public ChatGPTServiceImpl(OpenAIChatAPI openAIChatAPI, List<GPTFunctionsStub> stubs) throws Exception {
         this.openAIChatAPI = openAIChatAPI;
         for (GPTFunctionsStub functionStub : stubs) {
-            final Map<String, JsonSchemaFunction> functions = GPTFunctionUtils.extractFunctions(functionStub.getClass());
+            final Map<String, ChatGPTJavaFunction> functions = GPTFunctionUtils.extractFunctions(functionStub.getClass());
             if (!functions.isEmpty()) {
-                for (Map.Entry<String, JsonSchemaFunction> entry : functions.entrySet()) {
-                    final JsonSchemaFunction jsonSchemaFunction = entry.getValue();
+                for (Map.Entry<String, ChatGPTJavaFunction> entry : functions.entrySet()) {
+                    final ChatGPTJavaFunction jsonSchemaFunction = entry.getValue();
                     //set spring bean as target
                     jsonSchemaFunction.setTarget(functionStub);
                     allJsonSchemaFunctions.put(entry.getKey(), jsonSchemaFunction);
@@ -41,13 +41,6 @@ public class ChatGPTServiceImpl implements ChatGPTService {
     public Mono<ChatCompletionResponse> chat(ChatCompletionRequest request) {
         injectFunctions(request);
         boolean functionsIncluded = request.getFunctions() != null;
-        if (functionsIncluded) {
-            try {
-                System.out.println(GPTFunctionUtils.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(request));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
         if (!functionsIncluded) {
             return openAIChatAPI.chat(request);
         } else {
@@ -95,7 +88,7 @@ public class ChatGPTServiceImpl implements ChatGPTService {
         final FunctionCall functionCall = chatMessage.getFunctionCall();
         if (functionCall != null) {
             final String functionName = functionCall.getName();
-            JsonSchemaFunction jsonSchemaFunction = allJsonSchemaFunctions.get(functionName);
+            ChatGPTJavaFunction jsonSchemaFunction = allJsonSchemaFunctions.get(functionName);
             if (jsonSchemaFunction != null) {
                 functionCall.setFunctionStub(() -> jsonSchemaFunction.call(functionCall.getArguments())
                 );
