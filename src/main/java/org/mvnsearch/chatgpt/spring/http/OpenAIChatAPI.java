1b0fd7451ac67dcd1e986cf09c0c9a5efad48c9d
Libing Chen <libing.chen@gmail.com>
2023-06-20 21:46:35 +0800

    feat: add proxy() api

--- src/main/java/org/mvnsearch/chatgpt/spring/http/OpenAIChatAPI.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/http/OpenAIChatAPI.java
@@ -2,6 +2,7 @@ package org.mvnsearch.chatgpt.spring.http;
 
 import org.mvnsearch.chatgpt.model.ChatCompletionRequest;
 import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
+import org.reactivestreams.Publisher;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
@@ -15,5 +16,14 @@ public interface OpenAIChatAPI {
 
     @PostExchange("/chat/completions")
     Flux<ChatCompletionResponse> stream(@RequestBody ChatCompletionRequest request);
+
+    /**
+     * proxy to upstream
+     *
+     * @param request chat completion request
+     * @return response
+     */
+    @PostExchange("/chat/completions")
+    Publisher<ChatCompletionResponse> proxy(@RequestBody ChatCompletionRequest request);
 
 }
