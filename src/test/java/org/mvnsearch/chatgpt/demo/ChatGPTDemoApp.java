b1fb554b681da0f4d34c6668580ca0932574b60f
Libing Chen <libing.chen@gmail.com>
2023-06-22 15:17:04 +0800

    feat: add gptHelloService bean

--- src/test/java/org/mvnsearch/chatgpt/demo/ChatGPTDemoApp.java
+++ src/test/java/org/mvnsearch/chatgpt/demo/ChatGPTDemoApp.java
@@ -1,13 +1,21 @@
 package org.mvnsearch.chatgpt.demo;
 
+import org.mvnsearch.chatgpt.demo.service.GPTHelloService;
+import org.mvnsearch.chatgpt.spring.exchange.ChatGPTServiceProxyFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChatGPTDemoApp {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatGPTDemoApp.class, args);
+    }
+
+    @Bean
+    public GPTHelloService gptHelloService(ChatGPTServiceProxyFactory proxyFactory) {
+        return proxyFactory.createClient(GPTHelloService.class);
     }
 
 }
