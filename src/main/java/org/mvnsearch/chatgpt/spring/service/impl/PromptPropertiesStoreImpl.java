89e5f6bdec031bc568df32e025cd21c1df3b9e4b
Libing Chen <libing.chen@gmail.com>
2023-06-23 13:06:45 +0800

    feat: add support to load all prompts.properties file

--- src/main/java/org/mvnsearch/chatgpt/spring/service/impl/PromptPropertiesStoreImpl.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/service/impl/PromptPropertiesStoreImpl.java
@@ -1,24 +1,32 @@
 package org.mvnsearch.chatgpt.spring.service.impl;
 
 import org.mvnsearch.chatgpt.spring.service.PromptStore;
-import org.springframework.core.io.ClassPathResource;
 
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 
+/**
+ * load prompts from all prompts.properties files in classpath
+ *
+ * @author linux_china
+ */
 public class PromptPropertiesStoreImpl implements PromptStore {
     @Override
     public Map<String, String> readAll() {
         Map<String, String> prompts = new HashMap<>();
         try {
-            ClassPathResource classPathResource = new ClassPathResource("/prompts.properties");
-            if (classPathResource.exists()) {
-                Properties properties = new Properties();
-                properties.load(classPathResource.getInputStream());
-                properties.forEach((key, value) -> prompts.put(key.toString(), value.toString()));
+            Properties properties = new Properties();
+            final Enumeration<URL> promptURLs = this.getClass().getClassLoader().getResources("prompts.properties");
+            while (promptURLs.hasMoreElements()) {
+                try (var stream = promptURLs.nextElement().openStream()) {
+                    properties.load(stream);
+                }
             }
+            properties.forEach((key, value) -> prompts.put(key.toString(), value.toString()));
         } catch (Exception ignore) {
         }
         return prompts;
