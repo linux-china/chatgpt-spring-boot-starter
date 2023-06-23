521e4d724ddf17a80fe258e9d6681fe6d990e399
Libing Chen <libing.chen@gmail.com>
2023-06-23 11:09:03 +0800

    feat: add promptLambda() to build lambda

--- src/main/java/org/mvnsearch/chatgpt/spring/service/ChatGPTService.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/service/ChatGPTService.java
@@ -1,13 +1,19 @@
 package org.mvnsearch.chatgpt.spring.service;
 
+import org.jetbrains.annotations.PropertyKey;
 import org.mvnsearch.chatgpt.model.ChatCompletionRequest;
 import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+import static org.mvnsearch.chatgpt.spring.service.PromptManager.PROMPTS_FQN;
 
 public interface ChatGPTService {
     Mono<ChatCompletionResponse> chat(ChatCompletionRequest request);
 
     Flux<ChatCompletionResponse> stream(ChatCompletionRequest request);
 
+   <T> Function<T, Mono<String>> promptLambda(@PropertyKey(resourceBundle = PROMPTS_FQN) String promptKey);
 }
