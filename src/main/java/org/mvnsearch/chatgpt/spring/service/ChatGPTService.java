dcb04029e69a2ee405969b9f90edc9e8c613a1c9
Libing Chen <libing.chen@gmail.com>
2023-06-23 17:53:00 +0800

    chore: change to one function

--- src/main/java/org/mvnsearch/chatgpt/spring/service/ChatGPTService.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/service/ChatGPTService.java
@@ -6,7 +6,6 @@ import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.function.Function;
 
 import static org.mvnsearch.chatgpt.spring.service.PromptManager.PROMPTS_FQN;
@@ -18,5 +17,5 @@ public interface ChatGPTService {
 
     <T> Function<T, Mono<String>> promptAsLambda(@PropertyKey(resourceBundle = PROMPTS_FQN) String promptKey);
 
-    <T> Function<T, Mono<String>> promptAsLambda(@PropertyKey(resourceBundle = PROMPTS_FQN) String promptKey, List<String> functionNames);
+    <T, R> Function<T, Mono<R>> promptAsLambda(@PropertyKey(resourceBundle = PROMPTS_FQN) String promptKey, String functionName);
 }
