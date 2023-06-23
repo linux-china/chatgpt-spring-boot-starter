974d5022e9f1e64922a51e4afddb1fdcc96786f9
Libing Chen <libing.chen@gmail.com>
2023-06-23 11:31:13 +0800

    feat: add prompt template support

--- src/main/java/org/mvnsearch/chatgpt/model/ChatCompletion.java
+++ src/main/java/org/mvnsearch/chatgpt/model/ChatCompletion.java
@@ -1,12 +1,26 @@
 package org.mvnsearch.chatgpt.model;
 
+import org.jetbrains.annotations.PropertyKey;
+
 import java.lang.annotation.*;
+
+import static org.mvnsearch.chatgpt.spring.service.PromptManager.PROMPTS_FQN;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface ChatCompletion {
     String value() default "";
+
+    String system() default "";
+
+    String assistant() default "";
+
+    @PropertyKey(resourceBundle = PROMPTS_FQN) String userTemplate() default "";
+
+    @PropertyKey(resourceBundle = PROMPTS_FQN) String systemTemplate() default "";
+
+    @PropertyKey(resourceBundle = PROMPTS_FQN) String assistantTemplate() default "";
 
     String[] functions() default {};
 }
