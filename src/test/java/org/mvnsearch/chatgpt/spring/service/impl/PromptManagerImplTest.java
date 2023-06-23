be74cd502a832bc5c6042814c6dc4bb868d74e50
Libing Chen <libing.chen@gmail.com>
2023-06-23 10:07:00 +0800

    test: PromptManagerImplTest

---
+++ src/test/java/org/mvnsearch/chatgpt/spring/service/impl/PromptManagerImplTest.java
@@ -0,0 +1,20 @@
+package org.mvnsearch.chatgpt.spring.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.chatgpt.demo.ProjectBootBaseTest;
+import org.mvnsearch.chatgpt.spring.service.PromptManager;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PromptManagerImplTest extends ProjectBootBaseTest {
+    @Autowired
+    private PromptManager promptManager;
+
+    @Test
+    public void testLoadPrompt() {
+        assertThat(promptManager.prompt("sql-developer")).contains("SQL");
+        assertThat(promptManager.prompt("translate", "Chinese", "English")).contains("Chinese");
+    }
+}
