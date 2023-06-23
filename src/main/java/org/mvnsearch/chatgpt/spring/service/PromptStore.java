b4ec0a17d3c4e85b963e4729d5c55c08f399c2ea
Libing Chen <libing.chen@gmail.com>
2023-06-23 10:08:47 +0800

    app: PromptStore

---
+++ src/main/java/org/mvnsearch/chatgpt/spring/service/PromptStore.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.chatgpt.spring.service;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * prompts store interface
+ *
+ * @author linux_china
+ */
+public interface PromptStore {
+
+    Map<String, String> readAll() throws IOException;
+}
