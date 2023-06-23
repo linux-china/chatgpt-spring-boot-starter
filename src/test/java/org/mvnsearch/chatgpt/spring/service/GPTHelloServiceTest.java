0f5dc509c3b591013e0bb2dc5d991f977a1fe5ae
Libing Chen <libing.chen@gmail.com>
2023-06-23 12:13:00 +0800

    test: add testTranslateFromTemplate()

--- src/test/java/org/mvnsearch/chatgpt/spring/service/GPTHelloServiceTest.java
+++ src/test/java/org/mvnsearch/chatgpt/spring/service/GPTHelloServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.mvnsearch.chatgpt.demo.ProjectBootBaseTest;
 import org.mvnsearch.chatgpt.demo.service.GPTHelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Mono;
 
 
 public class GPTHelloServiceTest extends ProjectBootBaseTest {
@@ -17,6 +18,12 @@ public class GPTHelloServiceTest extends ProjectBootBaseTest {
 
     @Test
     public void testTranslate() throws Exception {
-        System.out.println(helloService.translate("Chinese", "English", "你好！").block());
+        final Mono<String> translate = helloService.translate("Chinese", "English", "你好！");
+        System.out.println(translate.block());
+    }
+
+    @Test
+    public void testTranslateFromTemplate() throws Exception {
+        System.out.println(helloService.translateFromTemplate("Chinese", "English", "你好！").block());
     }
 }
