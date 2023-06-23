99b9882baee1d11ec482912ff94029a3e8d2cab1
Libing Chen <libing.chen@gmail.com>
2023-06-23 12:00:40 +0800

    feat: add prompt template support

--- src/main/java/org/mvnsearch/chatgpt/spring/exchange/GptExchangeInvocationHandler.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/exchange/GptExchangeInvocationHandler.java
@@ -2,51 +2,72 @@ package org.mvnsearch.chatgpt.spring.exchange;
 
 import org.mvnsearch.chatgpt.model.*;
 import org.mvnsearch.chatgpt.spring.service.ChatGPTService;
+import org.mvnsearch.chatgpt.spring.service.PromptManager;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 
 public class GptExchangeInvocationHandler implements InvocationHandler {
     private final ChatGPTService chatGPTService;
+    private final PromptManager promptManager;
     private final Class<?> interfaceClass;
     private final GPTExchange gptExchangeAnnotation;
 
-    public GptExchangeInvocationHandler(ChatGPTService chatGPTService, Class<?> interfaceClass) {
+    public GptExchangeInvocationHandler(ChatGPTService chatGPTService, PromptManager promptManager, Class<?> interfaceClass) {
         this.chatGPTService = chatGPTService;
+        this.promptManager = promptManager;
         this.interfaceClass = interfaceClass;
         this.gptExchangeAnnotation = interfaceClass.getAnnotation(GPTExchange.class);
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String prompt;
+        List<ChatMessage> messages = new ArrayList<>();
         String[] functions = null;
         final ChatCompletion chatCompletionAnnotation = method.getAnnotation(ChatCompletion.class);
         if (chatCompletionAnnotation != null) {
-            prompt = chatCompletionAnnotation.value();
-            if (args != null && args.length > 0) {
-                if (prompt.contains("{") && prompt.contains("}")) {
-                    prompt = MessageFormat.format(prompt, args);
-                } else {
-                    StringBuilder sb = new StringBuilder(prompt);
-                    for (Object arg : args) {
-                        if (arg != null) {
-                            sb.append(" ").append(arg);
-                        }
-                    }
-                    prompt = sb.toString();
-                }
-            }
             functions = chatCompletionAnnotation.functions();
+            // user message
+            String userMessage = chatCompletionAnnotation.value();
+            if (userMessage.isEmpty() && !chatCompletionAnnotation.userTemplate().isEmpty()) {
+                userMessage = promptManager.prompt(chatCompletionAnnotation.userTemplate(), args);
+            } else {
+                userMessage = formatChatMessage("user", userMessage, args);
+            }
+            messages.add(ChatMessage.userMessage(userMessage));
+            // system message
+            String systemMessage = chatCompletionAnnotation.system();
+            if (systemMessage.isEmpty() && !chatCompletionAnnotation.systemTemplate().isEmpty()) {
+                systemMessage = promptManager.prompt(chatCompletionAnnotation.systemTemplate(), args);
+            } else if (!systemMessage.isEmpty()) {
+                systemMessage = formatChatMessage("system", systemMessage, args);
+            }
+            if (!systemMessage.isEmpty()) {
+                messages.add(ChatMessage.systemMessage(systemMessage));
+            }
+            // assistant message
+            String assistantMessage = chatCompletionAnnotation.assistant();
+            if (assistantMessage.isEmpty() && !chatCompletionAnnotation.assistantTemplate().isEmpty()) {
+                assistantMessage = promptManager.prompt(chatCompletionAnnotation.assistantTemplate(), args);
+            } else if (!assistantMessage.isEmpty()) {
+                assistantMessage = formatChatMessage("assistant", assistantMessage, args);
+            }
+            if (!assistantMessage.isEmpty()) {
+                messages.add(ChatMessage.assistantMessage(assistantMessage));
+            }
         } else {
-            prompt = method.getName();
-            prompt = prompt.replaceAll("([A-Z])", " $1").trim();
+            String userMessage = method.getName();
+            userMessage = userMessage.replaceAll("([A-Z])", " $1").trim();
+            messages.add(ChatMessage.userMessage(userMessage));
         }
         ChatCompletionRequest request = new ChatCompletionRequest();
-        request.addMessage(ChatMessage.userMessage(prompt));
+        request.setMessages(messages);
         // inject global configuration
         if (gptExchangeAnnotation != null) {
             if ((functions == null || functions.length == 0)) {
@@ -64,10 +85,33 @@ public class GptExchangeInvocationHandler implements InvocationHandler {
             if (gptExchangeAnnotation.value() != null && !gptExchangeAnnotation.value().isEmpty()) {
                 request.setModel(gptExchangeAnnotation.value());
             }
+            if (functions == null || functions.length == 0) {
+                functions = gptExchangeAnnotation.functions();
+            }
         }
         if (functions != null && functions.length > 0) {
             request.setFunctionNames(Arrays.stream(functions).toList());
+            return chatGPTService.chat(request).map(ChatCompletionResponse::getReplyCombinedText);
+        } else {
+            return chatGPTService.chat(request).map(ChatCompletionResponse::getReplyText);
         }
-        return chatGPTService.chat(request).map(ChatCompletionResponse::getReplyCombinedText);
+
+    }
+
+    public String formatChatMessage(String role, String content, Object[] args) {
+        if (args != null && args.length > 0) {
+            if (content.contains("{") && content.contains("}")) {
+                content = MessageFormat.format(content, args);
+            } else if (Objects.equals(role, "user")) {
+                StringBuilder sb = new StringBuilder(content);
+                for (Object arg : args) {
+                    if (arg != null) {
+                        sb.append(" ").append(arg);
+                    }
+                }
+                content = sb.toString();
+            }
+        }
+        return content;
     }
 }
