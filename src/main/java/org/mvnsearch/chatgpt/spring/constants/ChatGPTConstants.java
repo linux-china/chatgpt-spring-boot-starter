9e4983bf5c1d1ab7aac2e32aa193d348380be297
Libing Chen <libing.chen@gmail.com>
2023-06-26 09:39:40 +0800

    chore: code format with Spring Java Format

--- src/main/java/org/mvnsearch/chatgpt/spring/constants/ChatGPTConstants.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/constants/ChatGPTConstants.java
@@ -1,5 +1,7 @@
 package org.mvnsearch.chatgpt.spring.constants;
 
 public class ChatGPTConstants {
-    public static final String DEFAULT_MODEL = "gpt-3.5-turbo";
+
+	public static final String DEFAULT_MODEL = "gpt-3.5-turbo";
+
 }
