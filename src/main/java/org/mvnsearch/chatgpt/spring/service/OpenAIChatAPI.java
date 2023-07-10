c62b4c9451142641e46900afd34e865d9a9cfc98
Libing Chen <libing.chen@gmail.com>
2023-07-10 21:47:21 +0800

    feat: add embed and complete

--- src/main/java/org/mvnsearch/chatgpt/spring/service/OpenAIChatAPI.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/service/OpenAIChatAPI.java
@@ -1,7 +1,11 @@
 package org.mvnsearch.chatgpt.spring.service;
 
-import org.mvnsearch.chatgpt.model.ChatCompletionRequest;
-import org.mvnsearch.chatgpt.model.ChatCompletionResponse;
+import org.mvnsearch.chatgpt.model.completion.chat.ChatCompletionRequest;
+import org.mvnsearch.chatgpt.model.completion.chat.ChatCompletionResponse;
+import org.mvnsearch.chatgpt.model.completion.completion.CompletionRequest;
+import org.mvnsearch.chatgpt.model.completion.completion.CompletionResponse;
+import org.mvnsearch.chatgpt.model.embedding.EmbeddingsRequest;
+import org.mvnsearch.chatgpt.model.embedding.EmbeddingsResponse;
 import org.reactivestreams.Publisher;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.PostExchange;
@@ -23,5 +27,11 @@ public interface OpenAIChatAPI {
 	 */
 	@PostExchange("/chat/completions")
 	Publisher<ChatCompletionResponse> proxy(@RequestBody ChatCompletionRequest request);
+
+	@PostExchange("/embeddings")
+	Mono<EmbeddingsResponse> embed(@RequestBody EmbeddingsRequest request);
+
+	@PostExchange("/completions")
+	Mono<CompletionResponse> complete(@RequestBody CompletionRequest request);
 
 }
