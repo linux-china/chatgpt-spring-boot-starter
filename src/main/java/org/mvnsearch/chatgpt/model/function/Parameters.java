c0a3acef03962ab29cdb937896101fcd8a5900a5
Libing Chen <libing.chen@gmail.com>
2024-08-07 16:46:27 +0800

    app: parameters schema

---
+++ src/main/java/org/mvnsearch/chatgpt/model/function/Parameters.java
@@ -0,0 +1,177 @@
+package org.mvnsearch.chatgpt.model.function;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * parameters schema
+ * 
+ * @author linux_china
+ */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class Parameters {
+
+	private String type;
+
+	private Map<String, JsonSchemaProperty> properties;
+
+	private List<String> required;
+
+	private boolean additionalProperties = false;
+
+	public Parameters() {
+	}
+
+	public Parameters(String type, Map<String, JsonSchemaProperty> properties, List<String> required) {
+		this.type = type;
+		this.properties = properties;
+		this.required = required;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Map<String, JsonSchemaProperty> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Map<String, JsonSchemaProperty> properties) {
+		this.properties = properties;
+	}
+
+	public List<String> getRequired() {
+		return required;
+	}
+
+	public void setRequired(List<String> required) {
+		this.required = required;
+	}
+
+	public boolean isAdditionalProperties() {
+		return additionalProperties;
+	}
+
+	public void setAdditionalProperties(boolean additionalProperties) {
+		this.additionalProperties = additionalProperties;
+	}
+
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	public static class JsonSchemaProperty {
+
+		@JsonIgnore
+		private String name;
+
+		private String type;
+
+		private String description;
+
+		/**
+		 * items for type array
+		 */
+		private JsonArrayItems items;
+
+		/**
+		 * properties for object type
+		 */
+		private Map<String, JsonSchemaProperty> properties;
+
+		public JsonSchemaProperty() {
+		}
+
+		public JsonSchemaProperty(String name, String type, String description) {
+			this.name = name;
+			this.type = type;
+			this.description = description;
+		}
+
+		public JsonSchemaProperty(String name, String type, String description, JsonArrayItems items) {
+			this.name = name;
+			this.type = type;
+			this.description = description;
+			this.items = items;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public JsonArrayItems getItems() {
+			return items;
+		}
+
+		public void setItems(JsonArrayItems items) {
+			this.items = items;
+		}
+
+		public Map<String, JsonSchemaProperty> getProperties() {
+			return properties;
+		}
+
+		public void setProperties(Map<String, JsonSchemaProperty> properties) {
+			this.properties = properties;
+		}
+
+	}
+
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	public static class JsonArrayItems {
+
+		private String type;
+
+		private String description;
+
+		public JsonArrayItems() {
+		}
+
+		public JsonArrayItems(String type, String description) {
+			this.type = type;
+			this.description = description;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+	}
+
+}
