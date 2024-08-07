a3f601029e9be427e2fa82e25dd00a015a973112
Libing Chen <libing.chen@gmail.com>
2024-08-07 16:45:27 +0800

    app: ResponseFormat

---
+++ src/main/java/org/mvnsearch/chatgpt/model/completion/chat/ResponseFormat.java
@@ -0,0 +1,33 @@
+package org.mvnsearch.chatgpt.model.completion.chat;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * response_format for structured output
+ * 
+ * @author linux_china
+ */
+public class ResponseFormat {
+
+	private String type = "json_schema";
+
+	@JsonProperty("json_schema")
+	private ResponseFormatJsonSchema jsonSchema;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public ResponseFormatJsonSchema getJsonSchema() {
+		return jsonSchema;
+	}
+
+	public void setJsonSchema(ResponseFormatJsonSchema jsonSchema) {
+		this.jsonSchema = jsonSchema;
+	}
+
+}
