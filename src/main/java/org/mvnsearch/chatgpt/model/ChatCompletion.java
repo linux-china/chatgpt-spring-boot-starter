192a7afab7bbcd88438e9953a025553c4123ef16
Libing Chen <libing.chen@gmail.com>
2024-09-28 09:20:07 +0800

    chore: add javadoc

--- src/main/java/org/mvnsearch/chatgpt/model/ChatCompletion.java
+++ src/main/java/org/mvnsearch/chatgpt/model/ChatCompletion.java
@@ -1,6 +1,7 @@
 package org.mvnsearch.chatgpt.model;
 
 import org.jetbrains.annotations.PropertyKey;
+import org.springframework.core.annotation.AliasFor;
 
 import java.lang.annotation.*;
 
@@ -11,21 +12,56 @@ import static org.mvnsearch.chatgpt.spring.service.PromptManager.PROMPTS_FQN;
 @Documented
 public @interface ChatCompletion {
 
+	/**
+	 * user prompt, and placeholder is {0}, {1}, {2} fo method parameters
+	 * @return system prompt
+	 */
 	String value() default "";
 
+	/**
+	 * system prompt
+	 * @return system prompt
+	 */
 	String system() default "";
 
+	/**
+	 * user prompt, and placeholder is {0}, {1}, {2} fo method parameters
+	 * @return user prompt
+	 */
+	@AliasFor("value")
+	String user() default "";
+
+	/**
+	 * assistant prompt
+	 * @return assistant prompt
+	 */
 	String assistant() default "";
 
+	/**
+	 * user prompt template from prompts.properties
+	 * @return user prompt template
+	 */
 	@PropertyKey(resourceBundle = PROMPTS_FQN)
 	String userTemplate() default "";
 
+	/**
+	 * system prompt template from prompts.properties
+	 * @return system prompt template
+	 */
 	@PropertyKey(resourceBundle = PROMPTS_FQN)
 	String systemTemplate() default "";
 
+	/**
+	 * assistant prompt template from prompts.properties
+	 * @return assistant prompt template
+	 */
 	@PropertyKey(resourceBundle = PROMPTS_FQN)
 	String assistantTemplate() default "";
 
+	/**
+	 * function names annotated by @GPTFunction annotation
+	 * @return function names
+	 */
 	String[] functions() default {};
 
 }
