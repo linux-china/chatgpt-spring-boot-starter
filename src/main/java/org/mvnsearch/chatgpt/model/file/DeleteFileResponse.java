0675e9d32dfcbfad5586c948086228cbefb53916
Libing Chen <libing.chen@gmail.com>
2024-10-09 18:29:15 +0800

    app: delete file response

---
+++ src/main/java/org/mvnsearch/chatgpt/model/file/DeleteFileResponse.java
@@ -0,0 +1,39 @@
+package org.mvnsearch.chatgpt.model.file;
+
+/**
+ * delete file response
+ * 
+ * @author linux_china
+ */
+public class DeleteFileResponse {
+
+	private String id;
+
+	private String object;
+
+	private boolean deleted;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getObject() {
+		return object;
+	}
+
+	public void setObject(String object) {
+		this.object = object;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+}
