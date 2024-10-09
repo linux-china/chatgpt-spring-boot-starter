4e9e7696165c51390f9ec12d1ee7b8dbae6e9cf5
Libing Chen <libing.chen@gmail.com>
2024-10-09 18:30:09 +0800

    app: OpenAIFileAPI

---
+++ src/main/java/org/mvnsearch/chatgpt/spring/service/OpenAIFileAPI.java
@@ -0,0 +1,37 @@
+package org.mvnsearch.chatgpt.spring.service;
+
+import org.mvnsearch.chatgpt.model.file.DeleteFileResponse;
+import org.mvnsearch.chatgpt.model.file.FileObject;
+import org.mvnsearch.chatgpt.model.file.ListFilesResponse;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.PostExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * OpenAI File API
+ *
+ * @see <a href="https://platform.openai.com/docs/api-reference/files">OpenAI File API</a>
+ * @author linux_china
+ */
+public interface OpenAIFileAPI {
+
+	@PostExchange("/v1/files")
+	Mono<FileObject> upload(@RequestPart("purpose") String purpose, @RequestPart("file") Resource file);
+
+	@GetExchange("/v1/files")
+	Mono<ListFilesResponse> list();
+
+	@GetExchange("/v1/files/{file_id}")
+	Mono<FileObject> retrieve(@PathVariable("file_id") String fileId);
+
+	@DeleteExchange("/v1/files/{file_id}")
+	Mono<DeleteFileResponse> delete(@PathVariable("file_id") String fileId);
+
+	@GetExchange("v1/files/{file_id}/content")
+	Mono<byte[]> content(@PathVariable("file_id") String fileId);
+
+}
