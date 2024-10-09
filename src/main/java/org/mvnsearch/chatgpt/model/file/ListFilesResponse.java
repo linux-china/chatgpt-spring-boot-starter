5589d09afea7972aaf7a2ea8b5f2f534d20fa3e4
Libing Chen <libing.chen@gmail.com>
2024-10-09 18:29:56 +0800

    app: List files response

---
+++ src/main/java/org/mvnsearch/chatgpt/model/file/ListFilesResponse.java
@@ -0,0 +1,31 @@
+package org.mvnsearch.chatgpt.model.file;
+
+import java.util.List;
+
+/**
+ * List files response
+ * 
+ * @author linux_china
+ */
+public class ListFilesResponse {
+
+	private List<FileObject> data;
+
+	private String object;
+
+	public List<FileObject> getData() {
+		return data;
+	}
+
+	public void setData(List<FileObject> data) {
+		this.data = data;
+	}
+
+	public String getObject() {
+		return object;
+	}
+
+	public void setObject(String object) {
+		this.object = object;
+	}
+}
