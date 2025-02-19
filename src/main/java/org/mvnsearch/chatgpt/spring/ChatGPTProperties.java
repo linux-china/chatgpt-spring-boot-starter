f7d5f67c012f905bf8e486ebc4adc5ed3780ee96
Libing Chen <libing.chen@gmail.com>
2025-02-19 14:54:58 +0800

    chore: add model

--- src/main/java/org/mvnsearch/chatgpt/spring/ChatGPTProperties.java
+++ src/main/java/org/mvnsearch/chatgpt/spring/ChatGPTProperties.java
@@ -3,7 +3,7 @@ package org.mvnsearch.chatgpt.spring;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "openai")
-public record ChatGPTProperties(Api api) {
+public record ChatGPTProperties(Api api, String model) {
 
 	public record Api(String key, String url) {
 	}
